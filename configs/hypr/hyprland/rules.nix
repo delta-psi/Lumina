00d7315b621356f18f9d6906211dfc30679fa455
delta-psi <skorf60@gmail.com>
2026-01-03 16:07:07 -0500

    Updated rules.nix & misc.nix for updated hyprland

--- configs/hypr/hyprland/rules.nix
+++ configs/hypr/hyprland/rules.nix
@@ -1,57 +1,76 @@
 {...}: {
   windowrule = [
-    "opacity $windowOpacity override, fullscreen:0"
-    "opaque, class:foot|equibop|org\\.quickshell|imv|swappy"
-    "center 1, floating:1, xwayland:0"
-    "float, class:guifetch"
-    "float, class:yad"
-    "float, class:zenity"
-    "float, class:wev"
-    "float, class:org\\.gnome\\.FileRoller"
-    "float, class:file-roller"
-    "float, class:blueman-manager"
-    "float, class:com\\.github\\.GradienceTeam\\.Gradience"
-    "float, class:feh"
-    "float, class:imv"
-    "float, class:system-config-printer"
-    "float, class:org\\.quickshell"
-    "float, class:foot, title:nmtui"
-    "size 60% 70%, class:foot, title:nmtui"
-    "center 1, class:foot, title:nmtui"
-    "float, class:org\\.gnome\\.Settings"
-    "size 70% 80%, class:org\\.gnome\\.Settings"
-    "center 1, class:org\\.gnome\\.Settings"
-    "float, class:org\\.pulseaudio\\.pavucontrol|yad-icon-browser"
-    "size 60% 70%, class:org\\.pulseaudio\\.pavucontrol|yad-icon-browser"
-    "center 1, class:org\\.pulseaudio\\.pavucontrol|yad-icon-browser"
-    "float, class:nwg-look"
-    "size 50% 60%, class:nwg-look"
-    "center 1, class:nwg-look"
-    "workspace special:sysmon, class:btop"
-    "workspace special:music, class:feishin|Spotify|Supersonic|Cider"
-    "workspace special:music, initialTitle:Spotify( Free)?"
-    "workspace special:communication, class:discord|equibop|vesktop|whatsapp"
-    "workspace special:todo, class:Todoist"
-    "float, title:(Select|Open)( a)? (File|Folder)(s)?"
-    "float, title:File (Operation|Upload)( Progress)?"
-    "float, title:.* Properties"
-    "float, title:Export Image as PNG"
-    "float, title:GIMP Crash Debug"
-    "float, title:Save As"
-    "float, title:Library"
-    "move 100%-w-2% 100%-w-3%, title:Picture(-| )in(-| )[Pp]icture"
-    "keepaspectratio, title:Picture(-| )in(-| )[Pp]icture"
-    "float, title:Picture(-| )in(-| )[Pp]icture"
-    "pin, title:Picture(-| )in(-| )[Pp]icture"
-    "rounding 10, title:, class:steam"
-    "float, title:Friends List, class:steam"
-    "immediate, class:steam_app_[0-9]+"
-    "idleinhibit always, class:steam_app_[0-9]+"
-    "float, class:com-atlauncher-App, title:ATLauncher Console"
-    "noblur, title:Fusion360|(Marking Menu), class:fusion360\\.exe"
-    "nodim, xwayland:1, title:win[0-9]+"
-    "noshadow, xwayland:1, title:win[0-9]+"
-    "rounding 10, xwayland:1, title:win[0-9]+"
+
+    "opacity $windowOpacity override, match:fullscreen false"
+    "opaque true, match:class foot|equibop|org\\.quickshell|imv|swappy"
+    "center true, match:float true, match:xwayland false"
+
+    # Float
+    "float true, match:class guifetch"
+    "float true, match:class yad"
+    "float true, match:class zenity"
+    "float true, match:class wev"
+    "float true, match:class org\\.gnome\\.FileRoller"
+    "float true, match:class file-roller"
+    "float true, match:class blueman-manager"
+    "float true, match:class com\\.github\\.GradienceTeam\\.Gradience"
+    "float true, match:class feh"
+    "float true, match:class imv"
+    "float true, match:class system-config-printer"
+    "float true, match:class org\\.quickshell"
+
+    # Float, resize, & center
+    "float true, match:class foot, match:title nmtui"
+    "size 60% 70%, match:class foot, match:title nmtui"
+    "center 1, match:class foot, match:title nmtui"
+    "float true, match:class org\\.gnome\\.Settings"
+    "size 70% 80%, match:class org\\.gnome\\.Settings"
+    "center 1, match:class org\\.gnome\\.Settings"
+    "float true, match:class org\\.pulseaudio\\.pavucontrol|yad-icon-browser"
+    "size 60% 70%, match:class org\\.pulseaudio\\.pavucontrol|yad-icon-browser"
+    "center 1, match:class org\\.pulseaudio\\.pavucontrol|yad-icon-browser"
+    "float true, match:class nwg-look"
+    "size 50% 60%, match:class nwg-look"
+    "center 1, match:class nwg-look"
+
+    # Special Workspaces
+    "workspace special:sysmon, match:class btop"
+    "workspace special:music, match:class feishin|Spotify|Supersonic|Cider"
+    "workspace special:music, match:initial_title Spotify( Free)?"
+    "workspace special:communication, match:class discord|equibop|vesktop|whatsapp"
+    "workspace special:todo, match:class Todoist|Planify"
+
+    # Dialogs
+    "float true, match:title (Select|Open)( a)? (File|Folder)(s)?"
+    "float true, match:title File (Operation|Upload)( Progress)?"
+    "float true, match:title .* Properties"
+    "float true, match:title Export Image as PNG"
+    "float true, match:title GIMP Crash Debug"
+    "float true, match:title Save As"
+    "float true, match:title Library"
+
+    # Picture-in-picture (resize & move done via script)
+    "move 100%-w-2% 100%-w-3%, match:title Picture(-| )in(-| )[Pp]icture"
+    "keep_aspect_ratio true, match:title Picture(-| )in(-| )[Pp]icture"
+    "float true, match:title Picture(-| )in(-| )[Pp]icture"
+    "pin true, match:title Picture(-| )in(-| )[Pp]icture"
+
+    # Steam
+    "rounding 10, match:class steam"
+    "float true, match:title Friends List, class:steam"
+    "immediate true, match:class steam_app_[0-9]+"
+    "idle_inhibit always, match:class steam_app_[0-9]+"
+
+    # ATLauncher  console
+    "float true, match:class com-atlauncher-App, match:title ATLauncher Console"
+
+    # Autodesk Fusion 360
+    "no_blur, match:title Fusion360|(Marking Menu), match:class fusion360\\.exe"
+
+    # Ugh xwayland popups
+    "no_dim true, match:xwayland 1, match:title win[0-9]+"
+    "no_shadow true, match:xwayland 1, match:title win[0-9]+"
+    "rounding 10, match:xwayland 1, match:title win[0-9]+"
   ];
 
   workspace = [
@@ -60,15 +79,18 @@
   ];
 
   layerrule = [
-    "animation fade, hyprpicker"
-    "animation fade, logout_dialog"
-    "animation fade, selection"
-    "animation fade, wayfreeze"
-    "animation popin 80%, launcher"
-    "blur, launcher"
-    "noanim, caelestia-(border-exclusion|area-picker)"
-    "animation fade, caelestia-(drawers|background)"
-    "blur, caelestia-drawers"
-    "ignorealpha 0.57, caelestia-drawers"
+    "animation fade, match:namespace hyprpicker"
+    "animation fade, match:namespace logout_dialog"
+    "animation fade, match:namespace selection"
+    "animation fade, match:namespace wayfreeze"
+    
+    "animation popin 80%, match:namespace launcher"
+    "blur true, match:namespace launcher"
+
+    "no_anim true, match:namespace caelestia-(border-exclusion|area-picker)"
+    "animation fade, match:namespace caelestia-(drawers|background)"
+
+    "blur true, match:namespace caelestia-drawers"
+    "ignore_alpha 0.57, match:namespace caelestia-drawers"
   ];
 }
