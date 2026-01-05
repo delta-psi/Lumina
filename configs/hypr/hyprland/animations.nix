5c25aa22ca27ec90bca85f2daa1b8ba30c2f803f
delta-psi <skorf60@gmail.com>
2026-01-04 21:56:13 -0500

    Messing with some hyprland animations

--- configs/hypr/hyprland/animations.nix
+++ configs/hypr/hyprland/animations.nix
@@ -6,13 +6,16 @@
       "emphasizedAccel, 0.3, 0, 0.8, 0.15"
       "emphasizedDecel, 0.05, 0.7, 0.1, 1"
       "standard, 0.2, 0, 0, 1"
+
+      "easeOut, 0.16, 1, 0.3, 1"
     ];
     animation = [
       "layersIn, 1, 5, emphasizedDecel, slide"
       "layersOut, 1, 4, emphasizedAccel, slide"
       "fadeLayers, 1, 5, standard"
       "windowsIn, 1, 5, emphasizedDecel"
-      "windowsOut, 1, 3, emphasizedAccel"
+      # "windowsOut, 1, 3, emphasizedAccel"
+      "windowsOut, 1, 3, easeOut"
       "windowsMove, 1, 6, standard"
       "workspaces, 1, 5, standard"
       "specialWorkspace, 1, 4, specialWorkSwitch, slidefadevert 15%"
