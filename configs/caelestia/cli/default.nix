e7a1e705003de373ca2231de1251efcd04ada88f
delta-psi <skorf60@gmail.com>
2026-01-12 16:52:15 -0500

    Changed icon theme - hopefully this helps

--- configs/caelestia/cli/default.nix
+++ configs/caelestia/cli/default.nix
@@ -12,7 +12,8 @@
     # Theme config (only gtk for now since CLI's qt config depends on qt*ct-kde)
     home.packages = lib.optional mod.settings.theme.enableGtk pkgs.adw-gtk3;
     gtk = {
-      iconTheme.package = pkgs.papirus-icon-theme;
+      # iconTheme.package = pkgs.papirus-icon-theme;
+      iconTheme.package = pkgs.candy-icons
     };
   };
 }
